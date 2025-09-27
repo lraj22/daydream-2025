884f7a72b09d7d948386258845da80b01006d939
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-09-27 14:30:40 -0700

    complete and figure out logic; add and make level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -500.0
 
+# Get the position of a sprite node named "Player"
+@onready var player = $"."
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	#print("Player's local position: ", player.position)
+	#print("Player's global position: ", player.global_position)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
